09f67eb499a59c61ad0da4070291017f3261d256
ZenroySChance <zenroy@tiaoyueh.com.tw>
2024-07-27 03:34:29 +0800

    Feat: Header implementation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
-const header = () => {
-  return (
-    <header>
-        <main>
-            
-        </main>
-    </header>
-  )
+const Header = () => {
+    return (
+        <header className="resume-header">
+            <a href="/"><img src="http://localhost:5173/src/assets/profile_logo.png" alt="" width={64} height={64} /></a>
+            <span className="header-items-wrapper">
+                About
+            </span>
+            <span className="header-items-wrapper">
+                Work Experience
+            </span>
+            <span className="header-items-wrapper">
+                Skills
+            </span>
+            <span className="header-items-wrapper">
+                Project
+            </span>
+            <span className="header-items-wrapper">
+                Contact
+            </span>
+        </header>
+    )
 }
 
-export default header
+export default Header
