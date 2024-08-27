bfdb63491ac7e6d4625a5720301b1cd2fefde325
ZenroySChance <zenroy@tiaoyueh.com.tw>
2024-08-27 17:25:18 +0800

    Feat: Drawer Component added to Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Sections from './components/index';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import Contact from './components/Contact';
+import Drawer from './components/Drawer';
 
-const { Header, Hero, Skills, WorkExperience, Projects } = Sections;
+const { Hero, Skills, WorkExperience, Projects } = Sections;
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,16 +15,27 @@ function App() {
   const headerRef = useRef(null);
   const heroRef = useRef(null);
   const heroContainer = useRef(null);
+  const [drawerToggle, setDrawerToggle] = useState(false);
+  const drawerContainerRef = useRef(null);
+
+  useEffect(() => {
+    const drawerContainerElement: HTMLElement = drawerContainerRef.current!;
+    if (drawerContainerElement) {
+      drawerContainerElement.style.transform = drawerToggle ? 'scale(1)' : 'scale(0)';
+    }
+  }, [drawerToggle]);
+
+  const toggleDrawer = () => setDrawerToggle(!drawerToggle);
 
 
   useGSAP(() => {
-    const heroImg:HTMLElement = heroRef.current!;
-    const headerLogo:HTMLElement = headerRef.current!;
-    const headerPosition:DOMRect = headerLogo.getBoundingClientRect();
-    const heroImgPosition:DOMRect = heroImg.getBoundingClientRect();
+    const heroImg: HTMLElement = heroRef.current!;
+    const headerLogo: HTMLElement = headerRef.current!;
+    const headerPosition: DOMRect = headerLogo.getBoundingClientRect();
+    const heroImgPosition: DOMRect = heroImg.getBoundingClientRect();
 
     const mm = gsap.matchMedia();
-    
+
     const deltaX = headerPosition.left - heroImgPosition.left;
 
     if (heroImg && headerLogo) {
@@ -51,8 +63,23 @@ function App() {
   }, { scope: heroContainer, dependencies: [window.innerWidth] });
 
   return (
-    <div id="main-wrapper" className="relative w-screen">
-      <Header ref={headerRef} />
+    <div id="main-wrapper" className="relative w-screen flex flex-col">
+      <img
+        className="fixed mr-6 py-[calc(72px/2)] top-4 flex self-end z-50 cursor-pointer transition-transform ease-in-out duration-300 !mix-blend-difference hover:fill-green-500"
+        src={drawerToggle ? "/frontend/assets/web/icon_drawer_open.svg" : "/frontend/assets/web/icon_drawer_closed.svg"}
+        alt="Toggle Drawer"
+        height={32}
+        width={32}
+        onClick={toggleDrawer}
+        aria-label={drawerToggle ? "Close Drawer" : "Open Drawer"}
+        role="button"
+      />
+      <header className="resume-header">
+        <a href="/"><img className="ml-6 max-lg:hidden scale-0 relative z-50 !isolate" ref={headerRef} src="/frontend/assets/web/profile_logo.png" alt="Profile Logo" width={64} height={64} /></a>
+        <div ref={drawerContainerRef} id="drawer-container" className="scale-0 h-screen w-full absolute z-40 top-0 transform transition-transform ease-in-out origin-[calc(100%)_0rem] duration-500">
+          <Drawer setDrawerToggle={setDrawerToggle} drawerToggle={drawerToggle} />
+        </div>
+      </header>
       <main id="main-content" className="relative w-full flex flex-col justify-between items-center gap-8">
         <section ref={heroContainer}>
           <Hero ref={heroRef} />
@@ -67,10 +94,10 @@ function App() {
           <Projects />
         </section>
       </main>
-      <footer className="max-lg:mt-6">
+      <footer className="max-lg:mt-6 bg-white">
         <section id="contact-section" className="w-full flex flex-grow px-6">
-            <Contact />
-          </section>
+          <Contact />
+        </section>
       </footer>
     </div>
   );
